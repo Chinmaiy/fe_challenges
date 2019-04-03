4c4874b1a5685f07c883cc601389db46465326cd
Cezara Cbz <cezara.cbz@gmail.com>
2019-04-03 21:05:51 +0300

    Forkify - babel setup

--- forkify/webpack.config.js
+++ forkify/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['@babel/polyfill','./src/js/index.js'], //babel polyfill is used to provide ES5 implementations to things that were added in >=ES6 e.g. Promises
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -15,5 +15,16 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html' //could be automatically generated
         })
-    ]
+    ],
+    module: {
+        rules: [ //all the loaders - each an object
+            {
+                test: /\.js$/, //look for all files and test if ending in .js
+                exclude: /node_modules/,
+                use: { //for the files that match use this loader
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
