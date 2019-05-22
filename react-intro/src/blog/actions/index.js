00ffbc9fe3bde1b5bc2782778072d8d0ea9d2eb2
cobuzc <cobuzc@sky.optymyze.com>
2019-05-22 18:47:16 +0300

    blog project update (UserHeader component)

--- react-intro/src/blog/actions/index.js
+++ react-intro/src/blog/actions/index.js
@@ -47,6 +47,15 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
     dispatch({
         type: 'FETCH_POSTS',
-        payload: response
-    })
+        payload: response.data
+    });
 };
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+}
