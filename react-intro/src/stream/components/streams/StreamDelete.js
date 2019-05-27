28f609131be87ea0b35ee760209aee254afca317
cobuzc <cobuzc@sky.optymyze.com>
2019-05-27 18:25:49 +0300

    make Modal component more reusable

--- react-intro/src/stream/components/streams/StreamDelete.js
+++ react-intro/src/stream/components/streams/StreamDelete.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
+/**
+ * Use React.Fragment when you want to have multiple siblings elements not wrapped in a single
+ * explicit parent-element (which is a JSX requirement) which would be displayed in the DOM.
+ */
 const StreamDelete = () => {
+
+    const actions = (
+        <React.Fragment>
+            <button className="ui negative button">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    );
 
     return (
         <div>
-            StreamDelete
-            <Modal/>
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+                onDismiss={() => history.push("/")}
+            />
         </div>
     );
 };
