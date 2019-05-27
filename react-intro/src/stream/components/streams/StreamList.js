b2338d5d0336afd0cc0358fe2e1b46e987588324
cobuzc <cobuzc@sky.optymyze.com>
2019-05-27 18:59:20 +0300

    navigate to and display StreamShow component

--- react-intro/src/stream/components/streams/StreamList.js
+++ react-intro/src/stream/components/streams/StreamList.js
@@ -30,7 +30,7 @@ class StreamList extends React.Component {
                      {this.renderAdminButtons(stream) /*called here so semanticui to display correctly*/}  
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
