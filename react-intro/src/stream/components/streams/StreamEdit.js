327d2c81865fab58269876fb0df2d350092db53d
cobuzc <cobuzc@sky.optymyze.com>
2019-05-27 14:35:14 +0300

    navigate to StreamEdit

--- react-intro/src/stream/components/streams/StreamEdit.js
+++ react-intro/src/stream/components/streams/StreamEdit.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
+/**
+ * Every route/every component needs to fetch its own data 
+ * i.e. work in isolation, do NOT depend on previous state
+ * being set by some other component
+ */
 
-    return (
-        <div>StreamEdit</div>
-    );
+
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+
+        if(!this.props.stream) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                
+            </div>
+        );
+    };
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
