9b5f722a7efdde6aa5dbaa8925d033f825d084d8
cobuzc <cobuzc@sky.optymyze.com>
2019-06-10 18:45:47 +0300

    add create course form navigation button

--- app/src/components/courses/CreateCourse.js
+++ app/src/components/courses/CreateCourse.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import { Container, Header, Button, Divider } from 'semantic-ui-react';
+import { Container, Header, Button, Divider, Input, Label } from 'semantic-ui-react';
+import _ from 'lodash';
 
-import { EditableItemList } from '../generics';
+import { EditableItemList, FlexColumnContainer } from '../generics';
+
+import history from '../../history';
+
+const CourseBasicInfoForm = ({ onChange }) => {
+
+    return (
+        <FlexColumnContainer fluid>
+
+            <Input 
+                placeholder="Course Name"
+                size="big"
+                name="courseName"
+                onChange={onChange}
+            />
+
+        </FlexColumnContainer>
+    );
+}
 
 class CreateCourse extends React.Component {
 
     state = {
-        currentStep: 1,
+        currentStep: 0,
         courseName: '',
+        courseYear: null,
         courseComponents: []
     }
 
+    maxStepNr;
+
     render() {
 
-        const formStepNames = ["Basic Info", "Basic Components"];
+        const formStepNames = ["Basic Course Information", "Basic Components"];
+
+        const steps = {
+            0: <CourseBasicInfoForm
+                    onChange={this.onChange}
+                />,
+            1:  <EditableItemList 
+                    items={this.state.courseComponents} 
+                    placeholder="Component Name"
+                    onAddItem={this.onAddItem}
+                    onDeleteItem={this.onDeleteItem}
+                />
+        };
+
+        this.maxStepNr = _.max(Object.keys(steps).map(key => parseInt(key)));
 
         return (
             
@@ -24,21 +60,21 @@ class CreateCourse extends React.Component {
                     icon="settings" 
                     content={`Create Course - ${formStepNames[this.state.currentStep]}`}
                 />
-                <EditableItemList 
-                    items={this.state.courseComponents} 
-                    placeholder="Component Name"
-                    onAddItem={this.onAddItem}
-                    onDeleteItem={this.onDeleteItem}
-                />
+
+                {steps[this.state.currentStep]}               
 
                 <Divider/>
 
-                <Container fluid >
-                    <Button color="teal">Back</Button>
-                    <Button color="teal" floated="right">Next</Button>
-                </Container>
+                {this.renderNavigationButtons()}
             </Container>
         );
+    }
+
+    onChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
     }
 
     onAddItem = (itemName) => {
@@ -54,6 +90,26 @@ class CreateCourse extends React.Component {
         this.setState({
             courseComponents: this.state.courseComponents.filter(course => course.name !== itemName)
         });
+    }
+
+    renderNavigationButtons = () => {
+
+        return (
+            <Container fluid>
+                { this.state.currentStep > 0 ? <Button color="teal" onClick={() => this.setState({ currentStep: --this.state.currentStep })}>Back</Button> : null }
+
+                { 
+                    this.state.currentStep === this.maxStepNr ? 
+                    <Button color="teal" floated="right" onClick={this.onSaveBtnClick}>Save</Button> :
+                    <Button color="teal" floated="right" onClick={() => this.setState({ currentStep: ++this.state.currentStep })}>Next</Button>
+                }
+               
+            </Container>
+        );
+    }
+
+    onSaveBtnClick = () => {
+        history.push("/courses/create"); //todo navigate to newly created course
     }
 }
 
